ae198631d8766f922edf811e44a2c16e5b8dc9a8
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-11-29 10:06:37 +0600

    Setup navbar and footer

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Footer } from "./components/Footer";
+
+const App = () => {
+  return (
+    <div className="container mx-auto">
+      <Navbar />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
